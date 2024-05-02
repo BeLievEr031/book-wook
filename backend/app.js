3af5e44304d864893d2e45cd3577bb317272d67b
BeLieVer031 <sandyrajak031@gmail.com>
2024-05-02 09:01:22 +0530

    rate limiter added.

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import errorHandler from "./middleware/ErrorHandler.js";
 import userRouter from "./routes/user.route.js";
 import bookRouter from "./routes/book.route.js";
 import genreRouter from "./routes/genre.route.js";
+import rateLimit from "express-rate-limit"
 const app = express();
 app.use(cors({
     origin: config.FRONTEND_ORIGINS
@@ -15,6 +16,16 @@ app.use(cors({
 app.use(express.json({ limit: "16KB" }))
 app.use(express.urlencoded({ extended: true, limit: "16KB" }))
 app.use(cookieParser())
+
+// Define a rate limiter middleware
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per windowMs
+    message: 'Too many requests from this IP, please try again later'
+});
+
+// Apply the rate limiter to all requests
+app.use(limiter);
 
 // @Welcome Route
 app.use("/api/v1/welcome", (req, res) => {
