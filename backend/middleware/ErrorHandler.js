5750bbe562b764b9bb0065b7f145ca30844d504d
BeLieVer031 <sandyrajak031@gmail.com>
2024-05-02 18:48:47 +0530

    [BUG]: Invalid status bug fixed.

--- backend/middleware/ErrorHandler.js
+++ backend/middleware/ErrorHandler.js
@@ -1,10 +1,10 @@
 import config from "../config/config.js";
 
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    const statusCode = err.statusCode || 500;
     const message = err.message || "Something went wrong!!"
     res.status(statusCode).json({
-        status: statusCode <= 200,
+        status: false,
         message: message,
         err: config.NODE_ENV === 'production' ? null : err,
         stack: config.NODE_ENV === 'production' ? null : err.stack,
